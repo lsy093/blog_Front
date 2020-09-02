0e79b9285d1ac5017da90fbe9bf990c2010f684e
AlbertLeeeeee <836659259@qq.com>
2020-09-02 22:57:33 +0800

    增加带值跳转页面

--- src/views/form/index.vue
+++ src/views/form/index.vue
@@ -1,47 +1,23 @@
 <template>
   <div class="app-container">
-    <el-form ref="form" :model="form" label-width="120px">
-      <el-form-item label="Activity name">
-        <el-input v-model="form.name" />
+    <el-form ref="form" :model="form" label-width="100px">
+      <el-form-item label="姓名">
+        <!-- <span>{{this.$route.query.row.name}}</span> -->
+        <el-input :value="form.name" style="width: 20%;" />
       </el-form-item>
-      <el-form-item label="Activity zone">
-        <el-select v-model="form.region" placeholder="please select your zone">
-          <el-option label="Zone one" value="shanghai" />
-          <el-option label="Zone two" value="beijing" />
-        </el-select>
-      </el-form-item>
-      <el-form-item label="Activity time">
+      <el-form-item label="出生年月日">
         <el-col :span="11">
-          <el-date-picker v-model="form.date1" type="date" placeholder="Pick a date" style="width: 100%;" />
-        </el-col>
-        <el-col :span="2" class="line">-</el-col>
-        <el-col :span="11">
-          <el-time-picker v-model="form.date2" type="fixed-time" placeholder="Pick a time" style="width: 100%;" />
+          <el-date-picker type="date" placeholder="选择日期" v-model="form.date" style="width: 44%;"></el-date-picker>
         </el-col>
       </el-form-item>
-      <el-form-item label="Instant delivery">
-        <el-switch v-model="form.delivery" />
+      <el-form-item label="家庭住址">
+        <el-input v-model="form.address" style="width: 20%;" />
       </el-form-item>
-      <el-form-item label="Activity type">
-        <el-checkbox-group v-model="form.type">
-          <el-checkbox label="Online activities" name="type" />
-          <el-checkbox label="Promotion activities" name="type" />
-          <el-checkbox label="Offline activities" name="type" />
-          <el-checkbox label="Simple brand exposure" name="type" />
-        </el-checkbox-group>
-      </el-form-item>
-      <el-form-item label="Resources">
-        <el-radio-group v-model="form.resource">
-          <el-radio label="Sponsor" />
-          <el-radio label="Venue" />
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="Activity form">
-        <el-input v-model="form.desc" type="textarea" />
-      </el-form-item>
+
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">Create</el-button>
-        <el-button @click="onCancel">Cancel</el-button>
+        <el-button type="primary" @click="onSubmit">创建</el-button>
+        <el-button type="info" @click="empty">重置</el-button>
+        <el-button @click="onCancel">取消</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -52,34 +28,41 @@ export default {
   data() {
     return {
       form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
-      }
-    }
+        name: this.$route.query.row.name,
+        date: this.$route.query.row.date,
+        address: this.$route.query.row.address,
+      },
+    };
   },
   methods: {
     onSubmit() {
-      this.$message('submit!')
+      this.$message("submit!");
+      console.log(this.form.name)
+      console.log(this.form.date)
+      console.log(this.form.address)
+      console.log(this.$route.params.name)
     },
     onCancel() {
       this.$message({
-        message: 'cancel!',
-        type: 'warning'
-      })
-    }
-  }
-}
+        message: "cancel!",
+        type: "warning",
+      });
+    },
+    empty() {
+      this.form.name = "";
+      this.form.data = this.form.placeholder;
+      this.form.address = "";
+    },
+  },
+};
 </script>
 
 <style scoped>
-.line{
+.line {
   text-align: center;
+}
+.dialog-input-text >>> .el-input__inner {
+  width: 50px;
 }
 </style>
 
--- src/views/testTable/index.vue
+++ src/views/testTable/index.vue
@@ -8,13 +8,19 @@
       fit
       highlight-current-row
     >
-      <el-table-column prop="date" label="Date" width="150"></el-table-column>
-      <el-table-column prop="name" label="Name" width="150"></el-table-column>
-      <el-table-column prop="address" label="Address"></el-table-column>
+      <el-table-column prop="name" label="姓名" width="150"></el-table-column>
+      <el-table-column prop="date" label="出生日期" width="150"></el-table-column>
+      <el-table-column prop="address" label="家庭住址"></el-table-column>
+
       <el-table-column prop="button" label="Button" width="380">
-        <el-button type="primary" icon="el-icon-edit">修改</el-button>
+        <template slot-scope="{row,$index}">
+          <el-button type="primary" @click="Edit(row,$index)">修改</el-button>
+          <el-button type="danger" @click="delOffice(row.id)">删除</el-button>
+          <el-button type="primary" @click="addOffice(row)">添加下级机构</el-button>
+        </template>
+        <!-- <el-button type="primary" icon="el-icon-edit" @click="Edit">修改</el-button>
         <el-button type="primary" icon="el-icon-delete">删除</el-button>
-        <el-button type="primary" icon="el-icon-share">分享</el-button>
+        <el-button type="primary" icon="el-icon-share">分享</el-button> -->
       </el-table-column>
     </el-table>
   </div>
@@ -25,42 +31,52 @@ export default {
     return {
       tableData: [
         {
-          date: "2020-05-03",
-          name: "Tom",
-          address: "No. 189, Grove St, Los Angeles",
+          date: "2020-05-01",
+          name: "Tom1",
+          address: "No. 181, Grove St, Los Angeles",
         },
         {
           date: "2020-05-02",
-          name: "Tom",
-          address: "No. 189, Grove St, Los Angeles",
+          name: "Tom2",
+          address: "No. 182, Grove St, Los Angeles",
+        },
+        {
+          date: "2020-05-03",
+          name: "Tom3",
+          address: "No. 183, Grove St, Los Angeles",
         },
         {
           date: "2020-05-04",
-          name: "Tom",
-          address: "No. 189, Grove St, Los Angeles",
+          name: "Tom4",
+          address: "No. 184, Grove St, Los Angeles",
         },
         {
-          date: "2020-05-01",
-          name: "Tom",
-          address: "No. 189, Grove St, Los Angeles",
+          date: "2020-05-05",
+          name: "Tom5",
+          address: "No. 185, Grove St, Los Angeles",
         },
         {
-          date: "2020-05-01",
-          name: "Tom",
-          address: "No. 189, Grove St, Los Angeles",
+          date: "2020-05-06",
+          name: "Tom6",
+          address: "No. 186, Grove St, Los Angeles",
         },
         {
-          date: "2020-05-01",
-          name: "Tom",
-          address: "No. 189, Grove St, Los Angeles",
-        },
-        {
-          date: "2020-05-01",
-          name: "Tom",
-          address: "No. 189, Grove St, Los Angeles",
+          date: "2020-05-07",
+          name: "Tom7",
+          address: "No. 187, Grove St, Los Angeles",
         },
       ],
     };
+  },
+  methods: {
+    Edit(row) {
+      this.$router.push({
+        path: "/form/index",
+        query: {
+          row: row
+        },
+      });
+    },
   },
 };
 </script>
